a0e3e78e42f2bb254215c9c2e1ce191decdec234
howkat23 <howardkaterina20@gmail.com>
2023-07-15 17:54:16 -0400

    created hair-salon file

---
+++ script.js
@@ -0,0 +1,3 @@
+gsap.to('.one', { duration: 2, delay: 1, x: '-400', ease: 'power2.in', opacity: 0 })
+gsap.from('.two', { duration: 2, delay: 1.2, x: '-400', ease: 'power2.in', opacity: 0 })
+gsap.from('.heading', { duration: 2, delay: 2.5, y: 100, opacity: 0 })
